166a3a4a10a2f8ab4001262634504fc1899c5cfc
Rushikesh <143316081+RushiAbhi248@users.noreply.github.com>
2024-01-06 22:40:51 +0530

    Updated Day 02 progress

---
+++ Day_02/Diamond_pattern.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    int n = num/2;
+
+        for(int row=0; row<n; row=row+1) { //Full Pyramid
+        //space
+        for(int col=0; col<n-row-1; col=col+1) {
+            cout << " ";
+        }
+        //stars
+        for(int col=0; col<row+1; col=col+1) {
+            cout << "* ";
+        }
+        cout << endl;
+        }
+
+        for(int row=0; row<n; row=row+1) { //Inverted Full Pyramid
+        //spaces
+        for(int col=0; col<row; col=col+1) {
+            cout << " "; 
+        }
+        //stars
+        for(int col=0; col<n-row; col=col+1) {
+            cout << "* ";
+        }
+        cout << endl;
+        }
+
+
+    return 0;
+}
