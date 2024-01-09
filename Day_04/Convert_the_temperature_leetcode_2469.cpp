77d14726333ef2f3cfba05bd854fc702de37eca2
Rushikesh <143316081+RushiAbhi248@users.noreply.github.com>
2024-01-09 15:25:12 +0530

    Updated Day 04 progress

---
+++ Day_04/Convert_the_temperature_leetcode_2469.cpp
@@ -0,0 +1,13 @@
+//Convert the temperature (Leetcode problem no. 2469)
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double k = celsius + 273.15;
+        double f = celsius * 1.80 + 32.00;
+
+        vector<double> v;
+        v.push_back(k);
+        v.push_back(f);
+        return v;
+    }
+};
