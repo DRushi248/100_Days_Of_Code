6d690f07de0093d09c314fccc7c72945398e26b0
Rushikesh <143316081+RushiAbhi248@users.noreply.github.com>
2024-01-18 23:09:13 +0530

    Updated Day 14 progress

---
+++ Day_14/removeAllAdjacentDuplicatesInString_Leetcode_1047.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        int index = 0;
+
+        while(index < s.length()) {
+            if(ans.length()>0 && ans[ans.length()-1] == s[index]) {
+                //pop from ans string
+                ans.pop_back();
+            }
+            else {
+                //push
+                ans.push_back(s[index]);
+            }
+            index++;
+        }
+        return ans;
+    }
+};
