93b900341f7a27e669b676cf5d6371f58b5370d6
Rushikesh <143316081+RushiAbhi248@users.noreply.github.com>
2024-04-06 19:51:57 +0530

    Updated Day 93 progress

---
+++ Day_93/numberOfRecentCalls_leetcode_933.cpp
@@ -0,0 +1,25 @@
+class RecentCounter {
+public:
+    queue<int>q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        // 1.
+        q.push(t);
+
+        // 2. pop invalid req. older, older than t - 3000
+        while(!q.empty() && q.front() < (t - 3000))
+            q.pop();
+
+        // 3. now q has only number of recent calls : R = [t - 3000, t]
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
